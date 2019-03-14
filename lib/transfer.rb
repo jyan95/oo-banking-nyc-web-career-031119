2a621bbf42c0566ebbbad85c89eb6fac3d700960
Josh Yan <45473047+jyan95@users.noreply.github.com>
2019-03-14 11:19:06 -0400

    Update transfer.rb

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  attr_accessor  :status, :amount, :executed
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    @executed = false
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    if @status != "complete"
+      if self.valid? == true && @amount <= @sender.balance
+        @sender.balance -= amount
+        @receiver.balance += amount
+        @status = "complete"
+        @executed = true
+      else
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+  end
+
+  def reverse_transfer
+    if @executed == true
+      @sender.balance += amount
+      @receiver.balance -= amount
+      @status = "reversed"
+    end
+  end
 end
